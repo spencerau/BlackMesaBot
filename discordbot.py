17f8ca56901034d87537f2b8998713a2cc745846
Spencer Au <spencerau96@gmail.com>
2023-03-10 13:41:55 -0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,11 +1,10 @@
 import discord
-import youtube_dl
+#import youtube_dl
 import json
 import asyncio
-#import bitchute.py
 import requests
 import ffmpeg
-
+import re
 
 queue = []
 
@@ -31,15 +30,7 @@ async def on_message(message):
 
     if message.content.startswith('!play'):
         url = message.content.split()[1]
-        if 'youtube.com' or 'youtu.be' in url:
-            await message.channel.send('Adding video to queue...')
-            queue.append(url)
-            if not message.guild.voice_client or not message.guild.voice_client.is_connected():
-                voice_channel = message.author.voice.channel
-                await voice_channel.connect()
-            if not message.guild.voice_client.is_playing():
-                await play_video(message.guild)
-        elif 'bitchute.com' in url:
+        if 'youtube.com' or 'youtu.be' or 'bitchute.com' or 'goyimtv.com' in url:
             await message.channel.send('Adding video to queue...')
             queue.append(url)
             if not message.guild.voice_client or not message.guild.voice_client.is_connected():
@@ -52,7 +43,7 @@ async def on_message(message):
 
     if message.content == '!pause':
         if message.guild.voice_client and message.guild.voice_client.is_playing():
-            message.guild.voice_client.pause()
+            message.guild.voic_client.pause()
             await message.channel.send('Playback paused.')
         else:
             await message.channel.send('Nothing is playing at the moment.')
@@ -68,11 +59,9 @@ async def on_message(message):
 async def play_video(guild):
     while len(queue) > 0:
         url = queue.pop(0)
+        '''
         if 'youtube.com' or 'youtu.be' in url:
             voice_channel = guild.voice_client.channel
-            #await guild.voice_client.disconnect()
-            #await asyncio.sleep(1)
-            #await voice_channel.connect()
             await asyncio.sleep(1)
             await voice_channel.guild.change_voice_state(channel=voice_channel, self_mute=False, self_deaf=True)
             await voice_channel.send('Playing video from YouTube...')
@@ -83,25 +72,14 @@ async def play_video(guild):
                 guild.voice_client.play(source)
                 while guild.voice_client.is_playing():
                     await asyncio.sleep(1)
-        elif 'bitchute.com' in url:
-            play_bitchute_video(guild, url)
-            '''
-            voice_channel = guild.voice_client.channel
-            #await guild.voice_client.disconnect()
-            #await asyncio.sleep(1)
-            #await voice_channel.connect()
-            await asyncio.sleep(1)
-            await voice_channel.guild.change_voice_state(channel=voice_channel, self_mute=False, self_deaf=True)
-            await voice_channel.send('Playing video from BitChute...')
-            with youtube_dl.YoutubeDL() as ydl:
-                info = ydl.extract_info(url, download=False)
-                URL = info['url']
-                source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(URL))
-                guild.voice_client.play(source)
-                while guild.voice_client.is_playing():
-                    await asyncio.sleep(1)
-            '''
-    await guild.voice_client.disconnect()
+        '''
+        if 'bitchute.com' in url:
+            await play_bitchute_video(guild, url)
+
+        elif 'goyimtv.com' in url:
+            await play_goyimtv_video(guild, url)
+
+    #await guild.voice_client.disconnect()
 
 def get_bitchute_video_url(url):
     video_id = url.split('/')[-1]
@@ -129,6 +107,28 @@ async def play_bitchute_video(guild, url):
             await asyncio.sleep(1)
     else:
         await voice_channel.send('Unable to play video.')
-    #await guild.voice_client.disconnect()
+
+def get_goyimtv_video_url(url):
+    html = requests.get(url).text
+    regex = r'sources:\s*\[\s*\{\s*src:\s*"(.*?)"\s*,\s*type:\s*"video/mp4"\s*\}'
+    matches = re.search(regex, html)
+    if matches:
+        video_url = matches.group(1)
+        return video_url
+    return None
+
+async def play_goyimtv_video(guild, url):
+    voice_channel = guild.voice_client.channel
+    await voice_channel.guild.change_voice_state(channel=voice_channel, self_mute=False, self_deaf=True)
+    await voice_channel.send('Playing video from GoyimTV...')
+    video_url = get_goyimtv_video_url(url)
+    if video_url:
+        source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(video_url))
+        guild.voice_client.play(source)
+        while guild.voice_client.is_playing():
+            await asyncio.sleep(1)
+    else:
+        await voice_channel.send('Unable to play video.')
+
 
 client.run(key)
